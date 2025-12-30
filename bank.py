b40158fc8fdab07f2bd7bb9356a498e3b94c25f0
VaishnaviGanagi <vaishnavivaishu8484@gmail.com>
2025-12-30 11:49:42 +0530

    Added withdraw feature

--- bank.py
+++ bank.py
@@ -3,5 +3,12 @@ balance = float(input("Enter initial balance: "))
 deposit = float(input("Enter deposit amount: "))
 
 balance += deposit
+print("Balance after deposit:", balance)
 
-print("Updated balance after deposit:", balance)
+withdraw = float(input("Enter withdrawal amount: "))
+
+if withdraw <= balance:
+    balance -= withdraw
+    print("Balance after withdrawal:", balance)
+else:
+    print("Insufficient balance!")
